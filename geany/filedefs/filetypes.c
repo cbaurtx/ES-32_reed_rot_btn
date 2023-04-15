2f222853c07993a5b360604dca92cc15adf96d1e
Christof Baur <cbaurtx@gmx.net>
2023-04-16 00:28:03 +0200

    ä 	new file:   _clang-format 	modified:   components/reed_rot_btn/reed_rot_btn.c 	modified:   components/reed_rot_btn/ulp/reed_rot_btn.S 	modified:   geany/filedefs/filetypes.c 	modified:   geany/geany.conf 	new file:   geany/geany_socket_iris-aspire__0 	modified:   main/keypad_example_main.c

--- geany/filedefs/filetypes.c
+++ geany/filedefs/filetypes.c
@@ -5,6 +5,6 @@ FT_00_WD=
 FT_01_LB=_Build
 FT_01_CM=idf.py -C ~/Development/Source/ESP32/ESP32_reed_rot_btn/ build
 FT_01_WD=
-FT_02_LB=_Lint
-FT_02_CM=cppcheck --language=c --enable=warning,style --template=gcc "%f"
+FT_02_LB=Clang Format
+FT_02_CM=cd ~/Development//Source/ESP32/ESP32_reed_rot_btn; clang-format --style=file:_clang-format -i %d/%f
 FT_02_WD=
--- main/keypad_example_main.c
+++ main/keypad_example_main.c
@@ -2,56 +2,50 @@
  * @file    keypad_example_main.c
  * @author  Christof Baur
  * @date    09.04.2023
- * @version 0.9
+ * @version 0.92
  * @copyright   Christof Baur
  * @brief   ESP-32 library to interface with a rotary encoder and buttons
  *
  * License: MIT (see doc/LICENSE)
  */
 
-
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include "esp_log.h"
+#include "esp_sleep.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_sleep.h"
-#include "esp_log.h"
 
 #include "reed_rot_btn.h"
 
 static const char* TAG = "keypad_example_main.c";
 
-void app_main(void)
-{
+void app_main(void) {
     unsigned int key_code;
     static unsigned int key1_dly;
     esp_err_t err;
 
     reed_rot_btn_init();
 
-    for(;;) {
-      err = reed_rot_btn_read(&key_code, &key1_dly, CONFIG_ROT_KEY_TIMEOUT);
-      if(err == ESP_OK)
-        switch (key_code & 3) {
-          case REED_EVNT:
-            ESP_LOGI(TAG, "Reed event");
-            ESP_LOGI(TAG, "Reed delay %d", key1_dly);
-            break;
-          case BTN_EVNT:
-            ESP_LOGI(TAG, "Button event: %x", (key_code & 0xfffc) >> 2);
-            break;
-          case ROT_EVNT:
-            ESP_LOGI(TAG, "Turn event:  %d", key_code >> 0x10 );
-            break;
-          default:
-            ESP_LOGI(TAG, "Invalid");
+    for (;;) {
+        err = reed_rot_btn_read(&key_code, &key1_dly, CONFIG_ROT_KEY_TIMEOUT);
+        if (err == ESP_OK) switch (key_code & 3) {
+                case REED_EVNT:
+                    ESP_LOGI(TAG, "Reed event");
+                    ESP_LOGI(TAG, "Reed delay %d", key1_dly);
+                    break;
+                case BTN_EVNT:
+                    ESP_LOGI(TAG, "Button event: %x", (key_code & 0xfffc) >> 2);
+                    break;
+                case ROT_EVNT:
+                    ESP_LOGI(TAG, "Turn event:  %d", key_code >> 0x10);
+                    break;
+                default:
+                    ESP_LOGI(TAG, "Invalid");
+            }
+        if (err == ESP_ERR_TIMEOUT) ESP_LOGI(TAG, "Timed out\n");
 
-        }
-      if(err == ESP_ERR_TIMEOUT)
-        ESP_LOGI(TAG, "Timed out\n");
-
-      taskYIELD();
+        taskYIELD();
     }
 }
-
